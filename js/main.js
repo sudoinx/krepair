75eca5b8b18586a599182b31d9417f89f94d7505
st0mx <romangamers2009@gmail.com>
2025-09-19 22:17:26 +0300

    upgrade 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 const forms = [
-  document.getElementById("form-modal"),
+  document.getElementById("formModal"),
   document.getElementById("formMain"),
 ];
 
-function mainForm(event) {
-  event.preventDefault();
-
+document.getElementById("formMain").addEventListener("submit", function () {
   const name = document.getElementById("name");
   const phone = document.getElementById("phone");
   const mail = document.getElementById("email");
@@ -14,103 +12,89 @@ function mainForm(event) {
   phone.classList.remove("error");
   mail.classList.remove("error");
 
-  if (
-    name.value.trim() === "" ||
-    phone.value.trim() === "" ||
-    mail.value.trim() === ""
-  ) {
-    if (name.value.trim() === "") name.classList.add("error");
-    if (phone.value.trim() === "") phone.classList.add("error");
-    if (mail.value.trim() === "") mail.classList.add("error");
-    alert("Заполните все поля!");
-    return false;
-  }
+  let error = false;
 
-  if (phone.value.trim().length <= 5) {
+  if (name.value.trim() === "") {
+    name.classList.add("error");
+    error = true;
+  }
+  if (phone.value.trim() === "") {
     phone.classList.add("error");
-    alert("Номер телефона должен быть больше 5 символов!");
-    return false;
+    error = true;
   }
-
-  if (mail.value.trim().length <= 5) {
+  if (mail.value.trim() === "") {
     mail.classList.add("error");
-    alert("Email должен быть больше 5 символов!");
-    return false;
+    error = true;
   }
 
-  alert("Ваша заявка успешно отправлена!");
-  return true;
-}
-
-function mainForm() {
-  const name = document.getElementById("name");
-  const phone = document.getElementById("phone");
-  const mail = document.getElementById("email");
-
-  // Убираем старые ошибки
-  name.classList.remove("error");
-  phone.classList.remove("error");
-  mail.classList.remove("error");
-
-  if (
-    name.value.trim() === "" ||
-    mail.value.trim() === "" ||
-    phone.value.trim() === ""
-  ) {
-    if (name.value.trim() === "") name.classList.add("error");
-    if (phone.value.trim() === "") phone.classList.add("error");
-    if (mail.value.trim() === "") mail.classList.add("error");
-    alert("Заполните все поля!");
-    return false;
-  }
-
-  if (phone.value.trim().length <= 5) {
+  if (phone.value.trim().length > 0 && phone.value.trim().length < 5) {
     phone.classList.add("error");
-    alert("Номер телефона должен быть больше 5 символов!");
-    return false;
+    error = true;
   }
-
-  if (mail.value.trim().length <= 5) {
+  if (mail.value.trim().length > 0 && mail.value.trim().length < 5) {
     mail.classList.add("error");
-    alert("Email должен быть больше 5 символов!");
-    return false;
+    error = true;
   }
 
-  alert("Ваша заявка успешно отправлена!");
-  return true;
-}
+  if (!error) {
+    alert("Ваша заявка успешно отправлена!");
+    return true;
+  } else {
+    return false;
+  }
+});
 
-function modalForm() {
+document.getElementById("formModal").addEventListener("submit", function () {
   const name_modal = document.getElementById("name-modal");
   const phone_modal = document.getElementById("phone-modal");
-
-  let error = "";
+  const email_modal = document.getElementById("email-modal");
 
   name_modal.classList.remove("error");
   phone_modal.classList.remove("error");
+  email_modal.classList.remove("error");
 
-  if (name_modal.value === "" || phone_modal.value === "") {
-    error = "Заполните все поля!";
-    if (name_modal.value === "") name_modal.classList.add("error");
-    if (phone_modal.value === "") phone_modal.classList.add("error");
-  } else if (phone_modal.value.length <= 5) {
-    error = "Номер телефона должен быть больше 5 символов!";
-    phone_modal.classList.add("error");
+  let error = false;
+
+  if (name_modal.value.trim() === "") {
+    name_modal.classList.add("error");
+    error = true;
   }
 
-  if (error !== "") {
-    alert(error);
-    return false;
-  } else {
+  if (phone_modal.value.trim() === "") {
+    phone_modal.classList.add("error");
+    error = true;
+  }
+  if (email_modal.value.trim() === "") {
+    email_modal.classList.add("error");
+    error = true;
+  }
+
+  if (
+    phone_modal.value.trim().length > 0 &&
+    phone_modal.value.trim().length < 5
+  ) {
+    phone_modal.classList.add("error");
+    error = true;
+  }
+  if (
+    email_modal.value.trim().length > 0 &&
+    email_modal.value.trim().length < 5
+  ) {
+    email_modal.classList.add("error");
+    error = true;
+  }
+
+  if (!error) {
     alert("Ваша заявка успешно отправлена!");
     return true;
+  } else {
+    return false;
   }
-}
+});
 
 forms.forEach((form) => {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    location.reload();
   });
 });
 
